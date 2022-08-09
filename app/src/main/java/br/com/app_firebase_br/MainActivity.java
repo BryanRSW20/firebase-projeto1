b30fc99c2ecd11cdee4991a03194046b4588413f
ismaelsilvat <ismaelteixeiradas1@hotmail.com>
2022-08-09 09:47:20 -0300

    .

---
+++ app/src/main/java/br/com/app_firebase_br/MainActivity.java
@@ -0,0 +1,62 @@
+package br.com.app_firebase_br;
+
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+
+    private void batata(){
+        Map<String, Object> user = new HashMap<>();
+        user.put("first", "Ada");
+        user.put("last", "Lovelace");
+        user.put("born", 1815);
+
+
+        db.collection("users");
+        db.add(user);
+        db.addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+    }
+
+    // Create a new user with a first and last name
+
+// Add a new document with a generated ID
+
+}
---
+++ app/src/main/java/br/com/app_firebase_br/Texto.java
@@ -0,0 +1,22 @@
+package br.com.app_firebase_br;
+
+public class Texto {
+    private String conteudo;
+
+    public Texto(){
+
+
+    }
+
+    public Texto(String conteudo){
+        this.conteudo = conteudo;
+    }
+
+    public String getConteudo() {
+        return conteudo;
+    }
+
+    public void setConteudo(String conteudo) {
+        this.conteudo = conteudo;
+    }
+}
